434d63b3a64671f6feea1e7683cbe6db1e7537b3
Jim Stevenson <jimstevenson@Jims-MacBook-Pro.local>
2020-02-18 12:52:00 +0000

    add terrible functionality

--- divide.js
+++ divide.js
@@ -1,5 +1,8 @@
-const divideNum = (num, divisor) => {
-  return num / divisor;
+const divideNum = function(num, divisor) {
+  //this is how you protect against mutation right
+  const num2 = num;
+  const divisor2 = divisor;
+  return num2 / divisor2;
 };
 
 exports = { divideNum };
